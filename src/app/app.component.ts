bbaf24b0eca0801593f32e4ef23f9a4649d023de
Yesenia <eliyessie@gmail.com>
2018-04-13 09:31:09 -0700

    04.12.18- class assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+
 
 @Component({
 	selector: "application",
 	templateUrl: "./app.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+	
 	public userInput:String = "";
+
+	public waitForMe:Observable<boolean>
+
+	public previousAnswers: string[];
+
+	public onUserTypeSomething(event):void {
+		console.log(event);
+		this.userInput = event.target.value;
+	}
+	ngOnInit(): void {
+		this.previousAnswers = [];
+		this.waitForMe = Observable.timer(5000).map(() => true);
+	}
+
+	addToPrevious (answer:string): void {
+		this.previousAnswers.push(answer);
+	}
+	
 }
+
